ece8ee93ec2a9a213c786aca55aa9eefc79c9f51
Josh <wjosh60@yahoo.com>
2015-12-18 14:18:43 +0000

    Update pixelcc.js

--- pixelcc/pixelcc.js
+++ pixelcc/pixelcc.js
@@ -17,19 +17,3 @@ function noemail() {
 function offline() {
   alert("Forums Are Offline.")
 }
-
-window.onload = function() {
-  document.getElementById("LearnMoreBtn").onclick = function(){
-        var overlay = document.getElementById("overlay");
-        var popup = document.getElementById("popup");
-        overlay.style.display = "block";
-        popup.style.display = "block";
-    };
-    
-  document.getElementById("CloseBtn").onclick = function(){
-        var overlay = document.getElementById("overlay");
-        var popup = document.getElementById("popup");
-        overlay.style.display = "none";
-        popup.style.display = "none";      
-  }
-};
